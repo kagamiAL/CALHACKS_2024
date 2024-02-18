06f5bc2b50d5789f904e52dc5f38ac1b911ed80a
PastThePixels <pastthescreen@protonmail.com>
2024-02-18 15:23:50 -0700

    Add reset button in game

--- scenes/UI/GameOver.gd
+++ scenes/UI/GameOver.gd
@@ -1,7 +1,5 @@
 extends Control
 
-@onready var switch_scene = get_node("/root/SceneSwitch")
-
 @onready var time_label: Label = $CanvasLayer/VBoxContainer/Time as Label
 
 var player_time: float = 0;
@@ -11,8 +9,8 @@ func _ready():
 	time_label.text = "Time: " + str(player_time)
 
 func _on_main_menu_pressed():
-	switch_scene.goto_scene("res://scenes/Main/Main.tscn")
+	get_node("/root/SceneSwitch").goto_scene("res://scenes/Main/Main.tscn")
 
 
 func _on_restart_pressed():
-	switch_scene.goto_scene("res://scenes/Main/Game.tscn")
+	get_node("/root/SceneSwitch").goto_scene("res://scenes/Main/Game.tscn")
