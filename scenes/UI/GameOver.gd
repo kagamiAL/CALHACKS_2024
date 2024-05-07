8f77baf12e61d22fa2d958367ab461bbe12df8e5
PastThePixels <pastthescreen@protonmail.com>
2024-05-07 15:21:13 -0600

    Fix focus after game over

--- scenes/UI/GameOver.gd
+++ scenes/UI/GameOver.gd
@@ -7,6 +7,7 @@ var player_time: float = 0;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	time_label.text = "Time: " + str(player_time)
+	$CanvasLayer/VBoxContainer/Restart.grab_focus()
 
 func _on_main_menu_pressed():
 	get_node("/root/SceneSwitch").goto_scene("res://scenes/Main/Main.tscn")
