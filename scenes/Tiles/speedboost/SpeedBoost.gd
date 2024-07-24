3002fcdb13dacede14fab2d6ed4f4ae50d8b4274
PastThePixels <pastthescreen@protonmail.com>
2024-07-24 11:39:57 -0600

    Fix speed logic
    oh my god that sucked

--- scenes/Tiles/speedboost/SpeedBoost.gd
+++ scenes/Tiles/speedboost/SpeedBoost.gd
@@ -4,4 +4,8 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		body.linear_velocity += Vector2(speed, 0).rotated(global_rotation)
+		var rotated = Vector2(speed, 0).rotated(global_rotation)
+		if int(rotation_degrees) % 180 == 0:
+			body.linear_velocity.x = rotated.x
+		elif int(rotation_degrees) % 90 == 0:
+			body.linear_velocity.y = rotated.y
